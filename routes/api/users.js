3e534678164072cc1b3bcc7b80f211a35869a167
wxfeiang <wxfeiang@qq.com>
2019-01-23 11:53:28 +0800

    bcryptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
-
 
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
@@ -16,138 +16,132 @@ const validateLoginInput = require("../../validation/login");
 // @desc   返回的请求的json数据
 // @access public
 router.get("/test", (req, res) => {
-    res.json({
-        msg: "loworking "
-    })
-})
+  res.json({
+    msg: "loworking "
+  });
+});
 // $route  post api/users/test
 // @desc   返回的请求的json数据
-// @access public   注册接口 
+// @access public   注册接口
 router.post("/register", (req, res) => {
-    // console.log(req.body)  
-    // 查询数据库 
-    const {
-        errors,
-        isValid
-    } = validateRegisterInput(req.body); // 解构
-    // 判断是否通过
-    if (!isValid) {
-        return res.status(400).json(errors);
+  // console.log(req.body)
+  // 查询数据库
+  const { errors, isValid } = validateRegisterInput(req.body); // 解构
+  // 判断是否通过
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "邮箱已经被注册!!!"
+      });
+    } else {
+      // 头像地址
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      //存
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar //  es6一样只写一个
+      });
+      //  密码加密
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          // 保存
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
-
-
-    User.findOne({
-        email: req.body.email
-    }).then((user) => {
-        if (user) {
-            return res.status(400).json({
-                email: "邮箱已经被注册!!!"
-            })
-        } else {
-            // 头像地址
-            const avatar = gravatar.url(req.body.email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            });
-            //存
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                avatar, //  es6一样只写一个
-            })
-            //  密码加密
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    // 保存
-                    newUser.save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err))
-
-                });
-            });
-
-
-        }
-    })
-})
+  });
+});
 // $route  post api/users/login
 // @desc   jwt tocan
 // @access public 登录接口
 router.post("/login", (req, res) => {
-    const {
-        errors,
-        isValid
-    } = validateLoginInput(req.body); // 解构
-    // 判断isValid是否通过
-    if (!isValid) {
-        return res.status(400).json(errors);
+  const { errors, isValid } = validateLoginInput(req.body); // 解构
+  // 判断isValid是否通过
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  // 获取到
+  const email = req.body.email;
+  const password = req.body.password;
+  // 查询是否存在
+  User.findOne({
+    email
+  }).then(user => {
+    if (!user) {
+      return res.status(400).json({
+        email: "邮箱不存在!!!"
+      });
     }
+    // 密码匹配
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const rule = {
+          id: user.id,
+          name: user.name
+        };
+        //res.json({ msg: "sucess" })
+        // jwt.sign("规则","加密名字","tocken 过期时间","cb")
 
-    // 获取到 
-    const email = req.body.email;
-    const password = req.body.password;
-    // 查询是否存在
-    User.findOne({
-            email
-        })
-        .then((user) => {
-            if (!user) {
-                return res.status(400).json({
-                    email: "邮箱不存在!!!"
-                })
-            }
-            // 密码匹配
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const rule = {
-                            id: user.id,
-                            name: user.name
-                        };
-                        //res.json({ msg: "sucess" })
-                        // jwt.sign("规则","加密名字","tocken 过期时间","cb")
+        jwt.sign(
+          rule,
+          keys.secretOrKey,
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              sucess: true,
+              // token: "mrw" + token
+              token: "Bearer " + token //  固定格式
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({
+          password: "密码错误!!!"
+        });
+      }
+    });
+  });
+});
 
-                        jwt.sign(rule, keys.secretOrKey, {
-                            expiresIn: 3600
-                        }, (err, token) => {
-                            if (err) throw err;
-                            res.json({
-                                sucess: true,
-                                // token: "mrw" + token
-                                token: "Bearer " + token //  固定格式
-
-
-                            })
-                        })
-                    } else {
-                        return res.status(400).json({
-                            password: "密码错误!!!"
-                        })
-                    }
-                })
-
-        })
-})
-
-
-// $route  get api/users/current 
-// @desc   
-// @access public   请求当前信息    
+// $route  get api/users/current
+// @desc
+// @access public   请求当前信息
 //  验证token
-router.get("/current", passport.authenticate('jwt', {
+router.get(
+  "/current",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
+  }),
+  (req, res) => {
     // res.json(req.user)
     // res.json({ msg: "sucess" })
-    res.json({ //  之前pssport.js 上返回 
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email
+    res.json({
+      //  之前pssport.js 上返回
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
     });
-
-})
+  }
+);
 module.exports = router;
--- server.js
+++ server.js
@@ -7,40 +7,43 @@ const app = express();
 //users/.js
 const users = require("./routes/api/users");
 
-
 //DB
 const db = require("./config/keys").mongoURI;
-mongoose.connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log("数据库链接成功"))
-    .catch(err => console.log(err))
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("数据库链接成功"))
+  .catch(err => console.log(err));
 // Connect to mongodb
 
 // bodyParser 中间件使用
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // 使用中间件实现允许跨域
-app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Content-Type");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    next();
-  })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  next();
+});
 
-// 初始化 
-app.use(passport.initialize()); 
-require("./config/passport")(passport);  // 数据分离
+// 初始化
+app.use(passport.initialize());
+require("./config/passport")(passport); // 数据分离
 
 //  根路径
 app.get("/", (req, res) => {
-    res.send("这里是项目跟路径")
-})
+  res.send("这里是项目跟路径");
+});
 
-app.use("/api/users", users) // 上面引入进来的
+app.use("/api/users", users); // 上面引入进来的
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server  running  on prot  ${port}`)
-})
+  console.log(`Server  running  on prot  ${port}`);
+});
